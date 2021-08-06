e93a485e9f656fe9293200f680c008b6825d117f
Anthony <76069730+Smellynski@users.noreply.github.com>
2021-08-06 14:52:41 +0200

    Update TRG_music_bot.py

--- TRG_music_bot.py
+++ TRG_music_bot.py
@@ -9,6 +9,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
 
+## Play command (put url behind !play)
 @client.command
 async def play(ctx, url : str):
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
@@ -16,6 +17,7 @@ async def play(ctx, url : str):
     if not voice,is_connected():
         await voiceChannel.connect()
 
+## Disconnect the Bot      
 @client.command
 async def leave(ctx):
     voice=discord.utils.get(client.voice_clients, guild=ctx.guild)
@@ -24,6 +26,7 @@ async def leave(ctx):
     else:
         await ctx.send("Der Bot ist mit keinem Channel verbunden")
 
+#Pause the music
 @client.command
 async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
